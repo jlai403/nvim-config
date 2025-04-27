a7e07a5cea3cd61d10fa5788a0dbfd6677d61db3
jlai403 <joey@jlai.ca>
2025-04-27 10:53:12 -0600

    added sshconfig

--- main.zsh
+++ main.zsh
@@ -1,21 +1,27 @@
 #!/usr/bin/env zsh
 
-RED='\033[1;31m'
-GREEN='\033[1;32m'
-YELLOW='\033[1;33m'
-BLUE='\033[1;34m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+BRED='\033[1;31m'
+BGREEN='\033[1;32m'
+BYELLOW='\033[1;33m'
+BBLUE='\033[1;34m'
 NC='\033[0m' # No Color
 
 #################################
 # install brew app
 #################################
 
-echo "${GREEN}Installing Brew apps...${NC}"
+echo "${BGREEN}Installing Brew apps...${NC}"
 brew bundle install
 
 #################################
 # update .zshrc
 #################################
+echo "${BGREEN}Updating .zshrc...${NC}"
+
 if ! grep -q 'eval "$(starship init zsh)"' ~/.zshrc; then
   echo '
 # Load starship prompt
@@ -41,6 +47,28 @@ fi
 #################################
 # update dotfiles via symlinks
 #################################
+
+# ssh
+mkdir -p ~/.ssh
+
+# Only add Includes if not already present
+if ! grep -q "Include $(pwd)/ssh/\*.conf" ~/.ssh/config; then
+  cp ~/.ssh/config ~/.ssh/config.bak_$(date '+%Y%m%d')
+  echo "created backup of ~/.ssh/config -> ~/.ssh/config.bak_$(date '+%Y%m%d')"
+
+  includes=$(cat <<EOF
+Include $(pwd)/ssh/*.conf
+
+EOF
+)
+  tmpfile=$(mktemp)
+  echo "$includes" > "$tmpfile"
+  [ -f ~/.ssh/config ] && cat ~/.ssh/config >> "$tmpfile"
+  mv "$tmpfile" ~/.ssh/config
+  echo "${GREEN}Added Include $(pwd)/ssh/*.conf -> ~/.ssh/config${NC}"
+else
+  echo "${YELLOW}Include already present in ~/.ssh/config ${NC}"
+fi
 
 mkdir -p ~/.config
 
