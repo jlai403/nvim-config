33586b8cafd0f7fda14a7b96a19ffd5e6dbc3e16
Joey <jlai@stellaralgo.com>
2025-05-16 13:31:25 -0600

    docker-compose alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,6 +1,5 @@
 alias cls=clear
 
-
 # git aliases
 alias gi='git init'
 alias gcl='git clone'
@@ -15,11 +14,17 @@ alias gl="git log --all --graph --pretty=\
 	format:'%C(magenta)%h %C(white) %an %ar%C(auto) %D%n%s%n'"
 alias gb='git branch'
 
-# dev
+# aws
 function av() {
 	aws-vault exec "$1" -- zsh -i
 }
 
+# terragrunt
 alias tgo=terragrunt --terragrunt-tfpath tofu
 alias cleartf='rm -rf .terraform* .terragrunt-cache'
+
+# python
 alias svenv='source .venv/bin/activate'
+
+# docker
+alias dc='docker-compose'
